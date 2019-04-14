0492af0edaa56cf39df17904f65bdd86853bf289
zh <910442999@qq.com>
2019-04-15 03:37:28 +0800

    配置mybatis 和mybatis generator 自动生成代码插件

--- src/main/java/com/yc/mybatisgenerator/MybatisGeneratorApplication.java
+++ src/main/java/com/yc/mybatisgenerator/MybatisGeneratorApplication.java
@@ -1,9 +1,12 @@
 package com.yc.mybatisgenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.yc.mybatisgenerator.dao")
 public class MybatisGeneratorApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yc/mybatisgenerator/service/UserLoginService.java
@@ -0,0 +1,26 @@
+package com.yc.mybatisgenerator.service;
+
+import com.yc.mybatisgenerator.dao.UserMapper;
+import com.yc.mybatisgenerator.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserLoginService {
+
+//    /**
+//     * 注入dao
+//     */
+//    @Autowired
+//    private UserMapper usermapper;
+//
+//    //用户登录
+//    public User userLogin(String username, String password){
+//        User user = usermapper.selectByPrimaryKey(username);
+//        return user;
+//    }
+
+}
